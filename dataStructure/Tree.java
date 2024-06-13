2c9ff2ba802b7edfc24955e4096c88f761e909ae
gili-julio <giliardoplay@gmail.com>
2024-06-13 15:43:40 -0300

    :heavy_plus_sign: Tree: Método insert criado

--- dataStructure/Tree.java
+++ dataStructure/Tree.java
@@ -1,5 +1,8 @@
 package dataStructure;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Tree {
 
 	private Node root;
@@ -16,6 +19,31 @@ public class Tree {
 		
 		public boolean isLeaf() {
 			return (this.left == null) && (this.right == null);
+		}
+	}
+	
+	public void insert(int value) {
+		if(this.root == null) {
+			this.root = new Node(value);
+		} else {
+			Node newNode = new Node(value);
+			Queue<Node> queue = new LinkedList<>();
+			queue.add(this.root);
+			while(queue.size() > 0) {
+				Node currentNode = queue.remove();
+				if(currentNode.left == null) {
+					currentNode.left = newNode;
+					break;
+				} else {
+					queue.add(currentNode.left);
+				}
+				if(currentNode.right == null) {
+					currentNode.right = newNode;
+					break;
+				} else {
+					queue.add(currentNode.right);
+				}
+			}
 		}
 	}
 
