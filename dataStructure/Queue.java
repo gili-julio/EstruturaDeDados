561a84dfc5cafdab3889f74f4288d355c051e071
gili-julio <giliardoplay@gmail.com>
2024-06-13 15:12:04 -0300

    :heavy_plus_sign: Queue: Métodos getFirst e getLast criados

--- dataStructure/Queue.java
+++ dataStructure/Queue.java
@@ -2,6 +2,10 @@ package dataStructure;
 
 public class Queue {
 
+	private Node first;
+	private Node last;
+	private int length;
+	
 	public class Node {
 		int value;
 		Node next;
@@ -10,4 +14,29 @@ public class Queue {
 			this.value = value;
 		}
 	}
+	
+	public Queue(int value) {
+		Node newNode = new Node(value);
+		this.first = newNode;
+		this.last = newNode;
+		this.length++;
+	}
+	
+	public void getFirst() {
+		if(this.first != null) {
+			System.out.println("First: " + this.first.value);
+		} else {
+			System.out.println("Fila vazia");
+		}
+	}
+	
+	public void getLast() {
+		if(this.last != null) {
+			System.out.println("First: " + this.last.value);
+		} else {
+			System.out.println("Fila vazia");
+		}
+	}
+	
+	
 }
